d0c3a63c6e82661046c607b480f8a1ae8359cff6
Noelle <noellec3@illinois.edu>
2024-08-15 11:24:55 -0500

    pls work

--- src/routes/time.jsx
+++ src/routes/time.jsx
@@ -2,12 +2,36 @@ import React, { Component } from 'react'
 
 import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
-import { Scheduler, DayView, WeekView, MonthView, Appointments } from '@devexpress/dx-react-scheduler-material-ui';
+import {
+    Scheduler, WeekView, Appointments,
+    DateNavigator, TodayButton, Toolbar,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
-const currentDate = '2024-08-13';
-const schedulerData = [
-    { startDate: '2024-08-13T09:45', endDate: '2024-08-13T11:45', title: 'Example Activity' },
-];
+const projectColors = {
+    202873165: '#f0193d', // excercise
+    202873184: '#512638', // research
+    202873137: '#ff9909', // chores
+    202873108: '#ff6600', // progress narrative
+    202897898: '#7d7875', // misc.
+    null: '#3c3839', // none
+};
+
+const Appointment = ({
+  children, style, ...restProps
+}) => (
+  <Appointments.Appointment
+    {...restProps}
+    style={
+          {
+              ...style,
+              backgroundColor: children[1].props.data['color'],
+              borderRadius: '8px',
+          }
+      }
+  >
+    {children}
+  </Appointments.Appointment>
+);
 
 export default class Time extends Component {
 
@@ -15,34 +39,40 @@ export default class Time extends Component {
         super(props);
 
         this.state = {
-            month: 8,
-            year: 2024,
-            data: []
+            // month: 8,
+            // year: 2024,
+            data: [],
+            currentDate: '2024-08-13',
         };
     }
 
     async componentDidMount() {
-        const filename = 'toggl-' + this.state.year.toString() + '-' + this.state.month.toString() + '.json';
+        const month = 8;
+        const year = 2024;
+
+        const filename = 'toggl-' + year.toString() + '-' + month.toString() + '.json';
         let raw_data = await fetch(process.env.PUBLIC_URL + '/toggl/months/' + filename).then(response => response.json());
 
         let data = []
         for(const entry of raw_data) {
-            // console.log(entry);
-            data.push({startDate: entry['start'], endDate: entry['stop'], title: entry['description']});
+            data.push({startDate: entry['start'], endDate: entry['stop'], title: entry['description'], color: projectColors[entry['project_id']]});
         }
         console.log(data);
         this.setState({ data : data });
     }
 
 	render() {
-                        //<MonthView startDate={'2024-08-01'} />
 		return (
             <div>
                 <Paper>
                     <Scheduler data={this.state.data}>
-                        <ViewState currentDate={currentDate} />
-                        <WeekView startDate={'2024-08-11'} startDayHour={4} endDayHour={24} />
-                        <Appointments />
+                        <ViewState currentDate={this.state.currentDate} onCurrentDateChange={(newDate) => {this.setState({ currentDate : newDate })} }/>
+                        <WeekView startDayHour={4} endDayHour={24} />
+
+                        <Toolbar />
+                        <DateNavigator />
+                        <TodayButton />
+                        <Appointments appointmentComponent={Appointment}/>
                     </Scheduler>
                 </Paper>
             </div>
