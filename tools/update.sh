849a55eea8697bf735be4a0dee77f733ab77bb1a
Noelle <noellec3@illinois.edu>
2024-08-16 11:44:17 -0500

    trying to fix gitignore

--- tools/update.sh
+++ tools/update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-source .env/bin/activate
+# source .env/bin/activate
 
-python update_db.py
-python make_groups.py
+# toggl
+cd toggl
+python toggl_track.py
+cp -r months ../../public/toggl/
 
-cp menu_nodes.json ../progress-narrative/public/
-cp data.json ../progress-narrative/public/
+# python update_db.py
+# python make_groups.py
+
+# cp menu_nodes.json ../progress-narrative/public/
+# cp data.json ../progress-narrative/public/
