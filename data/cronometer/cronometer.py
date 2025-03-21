2366c4550037ef5cadf2647e37e402f9c204a1d9
Noelle Crawford <noellecrawfish@gmail.com>
2025-03-21 11:52:45 -0500

    pushing to github

--- data/cronometer/cronometer.py
+++ data/cronometer/cronometer.py
@@ -1,12 +1,14 @@
 
 import argparse
+import csv
 import os 
 
 import utils
 
+output_file = "cronometer.csv"
+
 def fetch_data(start_date:str) -> None:
     username, password = utils.get_secrets("cronometer")
-    output_file = "cronometer.csv"
 
     # $ is a special character to go, so may need to escape it
     password = password.replace("$", "\$")
@@ -15,10 +17,17 @@ def fetch_data(start_date:str) -> None:
     --startDate {start_date} --outputFile {output_file}"
     os.system(cmd)
 
+def format_data():
+    with open(f"cronometer/{output_file}", 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            print(row)
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--start_date", type=str, required=True)
     args = parser.parse_args()
 
-    fetch_data(args.start_date)
+    # fetch_data(args.start_date)
+    format_data()
 
